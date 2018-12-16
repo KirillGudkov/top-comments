0ee997b2369f843fb2fde9627e49ba6fb5f9f7ab
Kirill Gudkov <for.example.nickname@gmail.com>
2018-12-17 00:15:27 +0300

    Initial Commit

---
+++ Top Comments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Comments
+//
+//  Created by Kirill Gudkov on 17/12/2018.
+//  Copyright © 2018 Kirill Gudkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
